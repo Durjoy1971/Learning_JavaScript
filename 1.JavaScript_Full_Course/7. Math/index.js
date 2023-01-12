d4286f655743d586a1ab33a8eb2d5e865acc1694
Durjoy1971 <91456847+Durjoy1971@users.noreply.github.com>
2023-01-12 22:33:10 +0600

    Add files via upload

---
+++ 1.JavaScript_Full_Course/7. Math/index.js
@@ -0,0 +1,32 @@
+
+let x = 3.14;
+
+x = Math.round(x);
+console.log(x);
+
+x = Math.floor(x);
+console.log(x);
+
+x = Math.ceil(x);
+console.log(x);
+
+x = Math.pow(2,3);
+console.log(x);
+
+x = Math.sqrt(9);
+console.log(x);
+
+x = Math.abs(-5);
+console.log(x);
+
+let a = 5, b = 10;
+
+let maximum = Math.max(a,b);
+let minimum = Math.min(a,b);
+
+console.log("Maximum : ", maximum);
+console.log("Minimum : ", minimum);
+
+x = Math.PI;
+console.log(x);
+
