92f2ca3fad42152b5aaabcf63e40dc2d17a5d5ca
Durjoy1971 <91456847+Durjoy1971@users.noreply.github.com>
2023-01-13 16:11:23 +0600

    Add files via upload

---
+++ 1.JavaScript_Full_Course/24. Function/index.js
@@ -0,0 +1,8 @@
+// function = define code once, and use it many times.
+
+function happyBirthday(name){
+    console.log("Happy Birthday To " + name + " !!");
+}
+
+happyBirthday("Durjoy");
+happyBirthday("Joy");
