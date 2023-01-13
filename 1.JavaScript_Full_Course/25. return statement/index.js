b05c09ae0d1d9b68031c5ce12e9a20f05a552f53
Durjoy1971 <91456847+Durjoy1971@users.noreply.github.com>
2023-01-13 16:17:13 +0600

    Add files via upload

---
+++ 1.JavaScript_Full_Course/25. return statement/index.js
@@ -0,0 +1,14 @@
+// return = returns a value back to the place where you invoked a function
+
+let area, width, height;
+
+width = 5;
+height = 10;
+
+
+function getArea(width, height)
+{
+    return width*height;
+}
+
+console.log(getArea(width,height));
